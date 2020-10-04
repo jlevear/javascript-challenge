5f11878871010740617d50f847f6d53c13b18e6d
John Levear <jlevear@gmail.com>
2020-10-03 19:25:34 -0700

    adding completed homework

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,54 @@
-// from data.js
-var tableData = data;
+// Assign the data from `data.js` to a descriptive variable
+// Select the button
+// Select the form
 
-// YOUR CODE HERE!
+let ufoSightings = data;
+    button = d3.select("#button"),
+    form = d3.select("#form");
+
+console.log(ufoSightings)
+
+const tbody = d3.select("tbody");
+
+// Populate html table with the UFO Sighting data
+ufoSightings.forEach(ufoSighting => {
+    let row = tbody.append("tr");
+    Object.values(ufoSighting).forEach(value => {
+        let cell = row.append("td");
+        cell.text(value);
+    });
+});
+
+// Complete the event handler function for the form
+const runEnter = () => {
+
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+
+    // Select the input element and get the raw HTML node
+    // Get the value property of the input element
+    let inputElement = d3.select("#datetime"),
+        inputValue = inputElement.property("value");
+
+    let filteredData = ufoSightings.filter(sighting => sighting.datetime === inputValue)
+    
+    console.log(filteredData)
+
+    const tbody = d3.select("tbody");
+
+    // Clear existing table
+    tbody.html("");
+
+    filteredData.forEach(ufoSighting => {
+        let row = tbody.append("tr");
+        Object.values(ufoSighting).forEach(value => {
+            let cell = row.append("td");
+            cell.text(value);
+        });
+    });
+    
+}
+  
+// Create event handlers
+button.on("click", runEnter);
+form.on("submit", runEnter);
